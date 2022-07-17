c9c7bd409ac76521892aa50633215bd7d820a9cd
Norman_C <chewnorman@gmail.com>
2022-07-17 13:45:56 +0800

    .

--- Assets/Camera_controller.cs
+++ Assets/Camera_controller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Camera_controller : MonoBehaviour
 {
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Camera_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
 }

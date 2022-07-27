b90f843133f29155308d8c0f9a139259adc1d612
Norman_C <chewnorman@gmail.com>
2022-07-27 17:31:18 +0800

    .

---
+++ Assets/Dialogue/Test.ink
@@ -0,0 +1,2 @@
+﻿This is a test!
+This is the second line
--- Assets/Input_manager.cs
+++ Assets/Input_manager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
+public class Input_manager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

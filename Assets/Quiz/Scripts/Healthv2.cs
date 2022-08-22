bbb08f85ff6da187c663695e32b4844f006d4db8
dariusstan <dariustan0433@gmail.com>
2022-08-22 11:56:29 +0800

    .

--- Assets/Quiz/Scripts/Healthv2.cs
+++ Assets/Quiz/Scripts/Healthv2.cs
@@ -34,10 +34,12 @@
 // public string correctAns;           //correct option
 
 
+
 // using System;
 // using System.Collections;
 // using System.Collections.Generic;
 // using UnityEngine;
+
 // using UnityEngine.UI;
 
 // public class Healthv2 : MonoBehaviour
@@ -77,4 +79,22 @@
 
 //     }
 //     public string correctAns;  
+// }
+
+
+// public class Healthv2 : MonoBehaviour
+// {
+// #pragma warning disable 649
+//     //ref to the QuizGameUI script
+//     [SerializeField] private QuizGameUI ForHealth;
+//     //ref to the scriptableobject file
+//     [SerializeField] private List<Response> responseList;
+// #pragma warning restore 649
+
+//     private int gameScore;
+//     private int lifesRemaining;
+//     private Response responseList;
+
+   
+//     public List<Response> QuizData { get => responseList; }
 // }
